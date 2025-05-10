f4d0a8c47070acd299786a5cca1025d01def2e86
Cristian Sanchez <cristiansanchezsonido@gmail.com>
2025-05-10 19:42:05 +0100

    Added functionality to Background and Feats vignettes. Styles missing.

--- src/components/CharacterSheetViewer/Summary/PCBackground/PCBackground.tsx
+++ src/components/CharacterSheetViewer/Summary/PCBackground/PCBackground.tsx
@@ -1,15 +1,25 @@
 import styles from "../../CharacterSheetViewer.module.css"
+import { useContext } from "react"
+import { CharacterContext } from "../../CharacterSheetViewer"
+
 
 type Props = {
   customStyles?: React.CSSProperties
 }
 
 export default function PCBackground({ customStyles }: Props) {
+  const characterData = useContext(CharacterContext)
+  const background = characterData.background
+  
   return (
-    <div className={`${styles.vignette} row-start-1 row-end-2]`} style={customStyles}>
+    <div className={`${styles.vignette} row-start-1 row-end-2] flex flex-col`} style={customStyles}>
       <h2 className={styles.title}>
         Background
       </h2>
+      <h2 className="text-3xl py-3">{background.title}</h2>
+      <ul className="flex gap-2">{background.abilityScores.map((ability)=>(
+        <li key={ability}>{ability}</li>
+      ))}</ul>
     </div>
   )
 }
--- src/components/CharacterSheetViewer/Summary/PCFeats/PCFeats.tsx
+++ src/components/CharacterSheetViewer/Summary/PCFeats/PCFeats.tsx
@@ -1,15 +1,33 @@
 import styles from "../../CharacterSheetViewer.module.css"
+import { Accordion, AccordionTrigger, AccordionItem, AccordionContent } from "@/components/ui/accordion"
+import { useContext } from "react"
+import { CharacterContext } from "../../CharacterSheetViewer"
 
 type Props = {
   customStyles?: React.CSSProperties
 }
 
 export default function PCFeats({ customStyles }: Props) {
+  const characterData = useContext(CharacterContext)
+  const feats = characterData.feats
+
   return (
     <div className={`${styles.vignette} col-start-2 row-start-2 row-end-4`} style={customStyles}>
       <h2 className={styles.title}>
       Feats
       </h2>
+      <Accordion type="single" collapsible>
+      {feats.map(feat => (
+        <AccordionItem key={feat.title} value={feat.title}>
+          <AccordionTrigger>{feat.title}</AccordionTrigger>
+          <AccordionContent>
+            <p>{feat.description}</p>
+            <p>src: {feat.source}</p>
+            <p>ref: {feat.reference}</p>
+          </AccordionContent>
+        </AccordionItem>
+      ))}
+      </Accordion>
     </div>
   )
 }
