5a21ce0e07a2b805d79a27f7d45399e6f880fb01
Cristian Sanchez <cristiansanchezsonido@gmail.com>
2025-05-10 15:39:48 +0100

    Added the functionality to the Saving Throws vignette. Styling still missing.

--- src/components/CharacterSheetViewer/Abilities/PCSavingThrows/PCSavingThrows.tsx
+++ src/components/CharacterSheetViewer/Abilities/PCSavingThrows/PCSavingThrows.tsx
@@ -1,4 +1,5 @@
 import styles from "../../CharacterSheetViewer.module.css"
+import ProficiencyLine from "../ProficiencyLine/ProficiencyLine"
 
 type Props = {
   customStyles?: React.CSSProperties
@@ -9,8 +10,13 @@ export default function PCSavingThrows({ customStyles }: Props) {
     <div className={`${styles.vignette} col-start-2 row-start-1 row-end-3`} style={customStyles}>
       <h2 className={styles.title}>
         Saves
-        </h2>
-        
+      </h2>
+      <ProficiencyLine title="Strength" stat="Strength"/>
+      <ProficiencyLine title="Dexterity" stat="Dexterity"/>
+      <ProficiencyLine title="Constitution" stat="Constitution"/>
+      <ProficiencyLine title="Intelligence" stat="Intelligence"/>
+      <ProficiencyLine title="Wisdom" stat="Wisdom"/>
+      <ProficiencyLine title="Charisma" stat="Charisma"/>
     </div>
   )
 }
