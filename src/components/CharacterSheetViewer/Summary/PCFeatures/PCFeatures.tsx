6b0da13097b5a6bd3ebac08b94d73cf560192b95
Cristian Sanchez <cristiansanchezsonido@gmail.com>
2025-05-10 19:14:57 +0100

    Added functionality to Features, including the accordion view. Styling missing.

--- src/components/CharacterSheetViewer/Summary/PCFeatures/PCFeatures.tsx
+++ src/components/CharacterSheetViewer/Summary/PCFeatures/PCFeatures.tsx
@@ -1,15 +1,36 @@
 import styles from "../../CharacterSheetViewer.module.css"
+import { useContext } from "react"
+import { CharacterContext } from "../../CharacterSheetViewer"
+import { Accordion, AccordionItem, AccordionTrigger, AccordionContent } from "@/components/ui/accordion"
 
 type Props = {
   customStyles?: React.CSSProperties
 }
 
 export default function PCFeatures({ customStyles }: Props) {
+  const characterData = useContext(CharacterContext)
+  const featuresArray = characterData.features
   return (
     <div className={`${styles.vignette} col-start-3 row-start-1 row-end-4 `} style={customStyles}>
-      <h2 className={styles.title}>
-      Features
-      </h2>
+
+      <h2 className={styles.title}>Features</h2>
+
+      <Accordion type="multiple">
+        {featuresArray.map((feature) => (
+          <AccordionItem value={feature.title} key={feature.title} className="bg-gray-400">
+            <AccordionTrigger>{feature.title}</AccordionTrigger>
+            <AccordionContent>
+              <div className="bg-gray-200">
+                <p>{feature.description}</p>
+                <p>src: {feature.source}</p>
+                <p>ref: {feature.reference}</p>
+              </div>
+            </AccordionContent>
+          </AccordionItem>
+        )
+        )}
+
+      </Accordion>
     </div>
   )
 }
